62390df03c19c6daf420f6d8e3391a03588cd3f1
Xander van der Goot <xandervandergoot@gmail.com>
2025-10-24 17:36:34 +0800

    filter: Add command line interface

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,30 @@
+use argh::FromArgs;
 use profile_preprocessor::Profile;
 use std::{fs, io};
 
+#[derive(FromArgs)]
+#[argh(description = "Tool for taking filtering out frames from profile")]
+struct CMDLine {
+    #[argh(positional)]
+    input: String,
+    #[argh(positional)]
+    output: String,
+    #[argh(positional)]
+    regex: String,
+}
+
 fn main() -> Result<(), io::Error> {
-    let s = fs::read_to_string("./profile_prove.json")?;
+    let cmd: CMDLine = argh::from_env();
+
+    let s = fs::read_to_string(cmd.input)?;
     let mut content: Profile = serde_json::from_str(&s)?;
 
-    content.exclude_function("rayon");
+    content.exclude_function(&cmd.regex);
 
     // statistics?
     println!("weights: {:?}", content.total_samples());
 
-    fs::write(
-        "./profile_prove_flattened.json",
-        serde_json::to_string_pretty(&content)?,
-    )?;
+    fs::write(cmd.output, serde_json::to_string_pretty(&content)?)?;
 
     Ok(())
 }
