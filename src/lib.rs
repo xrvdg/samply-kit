ef74ad87b1f3a926a39c19981afaa29a4dc1c43b
Xander van der Goot <xandervandergoot@gmail.com>
2025-10-27 10:59:10 +0800

    TypedVec and Indicies: IndexStackTable

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
-use std::collections::{BTreeMap, HashSet};
+use std::{
+    collections::{BTreeMap, HashSet},
+    marker::PhantomData,
+    ops::{Index, IndexMut},
+};
 
 use itertools::{self, Itertools};
 use regex::Regex;
@@ -9,11 +13,11 @@ impl StackTable {
     /// Squash out excluded prefixes.
     fn squash_excluded_parents(
         &mut self,
-        id: IndexToStackTable,
-        excluded: &HashSet<IndexToStackTable>,
+        id: Id<IndexStackTable>,
+        excluded: &HashSet<Id<IndexStackTable>>,
     ) {
         if let Some(prefix_id) = self.prefix[id] {
-            if excluded.contains(&prefix_id) {
+            if excluded.iter().contains(&prefix_id) {
                 self.prefix[id] = self.prefix[prefix_id];
                 self.squash_excluded_parents(id, excluded);
             }
@@ -22,9 +26,16 @@ impl StackTable {
 
     /// Rewrite such that non-excluded frames do not point at excluded frames anymore.
     /// Excluded frames themselves stay included to not mess up the indexing and they act as a fast way to
-    fn exclude(&mut self, excluded: &HashSet<IndexToStackTable>) {
+    fn exclude(&mut self, excluded: &HashSet<Id<IndexStackTable>>) {
         for i in 0..self.length {
-            self.squash_excluded_parents(i, excluded);
+            self.squash_excluded_parents(
+                // TODO better way to do this
+                Id {
+                    idx: i,
+                    _marker: PhantomData,
+                },
+                excluded,
+            );
         }
     }
 }
@@ -32,6 +43,42 @@ impl StackTable {
 struct Edge {
     caller: IndexToFuncTable,
     callee: IndexToFuncTable,
+}
+
+// From I to T
+#[derive(Clone, Debug, Serialize, Deserialize)]
+struct TypedVec<I, T> {
+    inner: Vec<T>,
+    _marker: PhantomData<I>,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Copy, Eq, Hash)]
+struct Id<I> {
+    idx: usize,
+    _marker: PhantomData<I>,
+}
+
+impl<I> Id<I> {
+    fn new(id: usize) -> Id<I> {
+        Id {
+            idx: id,
+            _marker: PhantomData,
+        }
+    }
+}
+
+impl<I, T> Index<Id<I>> for TypedVec<I, T> {
+    type Output = T;
+
+    fn index(&self, index: Id<I>) -> &Self::Output {
+        &self.inner[index.idx]
+    }
+}
+
+impl<I, T> IndexMut<Id<I>> for TypedVec<I, T> {
+    fn index_mut(&mut self, index: Id<I>) -> &mut Self::Output {
+        &mut self.inner[index.idx]
+    }
 }
 
 // Can I make an index over the table and then do the access?
@@ -47,7 +94,7 @@ impl Thread {
     // We need edges
     // For now frame is mostly an indirection
     // Building the path this way is fine and then grouping it by 2
-    fn path(&self, id: IndexToStackTable) -> Vec<IndexToFuncTable> {
+    fn path(&self, id: Id<IndexStackTable>) -> Vec<IndexToFuncTable> {
         let stack = &self.stack_table;
         let frame = &self.frame_table;
         let mut p = match stack.prefix[id] {
@@ -62,7 +109,11 @@ impl Thread {
         let stack = &self.stack_table;
         let mut p = Vec::with_capacity(stack.length);
         for i in 0..stack.length {
-            p.push(self.path(i));
+            // TODO better
+            p.push(self.path(Id {
+                idx: i,
+                _marker: PhantomData,
+            }));
         }
         p
     }
@@ -86,11 +137,13 @@ impl Thread {
             .positions(|id| exclude_func_table.contains(id))
             .collect();
 
-        let exclude_stack_table: HashSet<_> = self
+        let exclude_stack_table: HashSet<Id<IndexStackTable>> = self
             .stack_table
             .frame
+            .inner
             .iter()
             .positions(|id| exclude_frame_table.contains(id))
+            .map(|pos| Id::new(pos))
             .collect();
 
         self.stack_table.exclude(&exclude_stack_table);
@@ -101,9 +154,9 @@ impl Thread {
     /// Samples that point to an excluded stack entry needs to be reassigned to its parent.
     ///
     /// Run this after stack.exclude to prevent reassing the sample to another excluded stack entry
-    fn reattribute_samples(&mut self, excluded: &HashSet<IndexToStackTable>) {
-        for s in &mut self.samples.stack {
-            if excluded.contains(s) {
+    fn reattribute_samples(&mut self, excluded: &HashSet<Id<IndexStackTable>>) {
+        for s in &mut self.samples.stack.inner {
+            if excluded.iter().contains(s) {
                 if let Some(prefix) = self.stack_table.prefix[*s] {
                     *s = prefix
                 }
@@ -137,17 +190,18 @@ impl Profile {
     }
 }
 
+//TODO remove inners
+
 impl SampleTable {
     fn total_weight(&self) -> usize {
         match &self.weight {
             Some(weights) => weights.iter().sum(),
             // Weights is assumed to be 1
-            None => self.stack.len(),
+            None => self.stack.inner.len(),
         }
     }
 }
 
-type IndexToStackTable = usize;
 type IndexToFrameTable = usize;
 type IndexToFuncTable = usize;
 type IndexToStringTable = usize;
@@ -173,17 +227,22 @@ struct Thread {
     other: BTreeMap<String, Value>,
 }
 
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq, Copy, Hash)]
+struct IndexSampleTable;
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq, Copy, Hash)]
+struct IndexStackTable;
+
 #[derive(Clone, Debug, Serialize, Deserialize)]
 struct SampleTable {
-    stack: Vec<IndexToStackTable>,
+    stack: TypedVec<IndexSampleTable, Id<IndexStackTable>>,
     weight: Option<Vec<usize>>,
     #[serde(flatten)]
     other: BTreeMap<String, Value>,
 }
 #[derive(Clone, Debug, Serialize, Deserialize)]
 struct StackTable {
-    prefix: Vec<Option<IndexToStackTable>>,
-    frame: Vec<IndexToFrameTable>,
+    prefix: TypedVec<IndexStackTable, Option<Id<IndexStackTable>>>,
+    frame: TypedVec<IndexStackTable, IndexToFrameTable>,
     length: usize,
     #[serde(flatten)]
     other: BTreeMap<String, Value>,
